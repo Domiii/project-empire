8c77bd8569bd45dbf0eb32242a2e5b35b2c2be1e
Domi <Domiii@users.noreply.github.com>
2018-03-17 22:33:46 +0800

    Update ReflectionForm.js

--- src/views/components/reflection/ReflectionForm.js
+++ src/views/components/reflection/ReflectionForm.js
@@ -21,12 +21,28 @@ import { EmptyObject } from '../../../util';
 /**
  * TODO
   * Goal: Evaluate and optimize learning path, problem solving + collaboration
+  * Goal: Create a proper method of (self-) evaluation of learning that can be as an objective evaluation method to replace other grading and assessment schemes?
+    * Arguing on your own behalf? (Against yourself?)
+    * Peers + trust?
   * coachee can customize own form
   * coach can also customize everyone's form
   * coach/coachee can see/visualize own statistics
   * coach can produce overall statistics
   * coach gives feedback on things
   * coach/coachee can send follow-up questions
+  
+  Co-Learning System Ideas
+  Feature: Checklists
+  Feature: Individually customizable Checklists
+  Feature: Pub-sub for "units of learning" (concepts, problems, questions etc)
+    * Weekly subbing for in-class grouping
+    * Point system for pub-sub
+    * Problem: Everyone needs to have the most fitting version of the interpretation of the point system?
+    * Problem: Points for different units have different values?
+    * Problem: Gotta be able to get multiple points for performing a unit in different ways/revisiting it (e.g. finiding multiple ways to download website)
+    * Problem: Point computation must become straight-forward
+      * NOTEL might take a while before one realizes how well a concept is understood!
+  Feature: Point collection needs to lead something more (since system usage is not open-ended and generally ends (latest) with the semester)
  */
 
 export const schemaTemplate = {
